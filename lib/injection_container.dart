4afa358e261ab6cdeaeee8c5b2a521ee061f6850
ShallowCode <asfak7074@gmail.com>
2024-05-17 21:47:13 +0600

    feat: Add injection container for dependency management

---
+++ lib/injection_container.dart
@@ -0,0 +1,35 @@
+import 'package:get_it/get_it.dart';
+// ignore: depend_on_referenced_packages
+import 'package:dio/dio.dart';
+
+import 'package:bloc_news_app/features/daily_news/data/repository/article_repository_impl.dart';
+import 'package:bloc_news_app/features/daily_news/data/data_sources/news_api_service.dart';
+import 'package:bloc_news_app/features/daily_news/domain/repository/article_repository.dart';
+import 'package:bloc_news_app/features/daily_news/domain/use_cases/fetch_article.dart';
+import 'package:bloc_news_app/features/daily_news/presentation/bloc/remote/remote_article_bloc.dart';
+
+final sl = GetIt.instance;
+
+Future<void> initializeDependencies() async {
+  // dio
+  sl.registerSingleton<Dio>(Dio());
+
+  // dependencies
+  sl.registerSingleton<NewsApiService>(
+    NewsApiService(sl<Dio>()),
+  );
+
+  sl.registerSingleton<ArticleRepository>(
+    ArticleRepositoryImpl(sl<NewsApiService>()),
+  );
+
+  // use cases
+  sl.registerSingleton<FetchArticleUseCase>(
+    FetchArticleUseCase(sl<ArticleRepository>()),
+  );
+
+  // blocs
+  sl.registerFactory<RemoteArticleBloc>(
+    () => RemoteArticleBloc(sl<FetchArticleUseCase>()),
+  );
+}
