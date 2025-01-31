d5ea684a94015da6eb829f1b8b46f7de02b758d5
KayesAshfaq <asfak7074@gmail.com>
2025-01-31 11:53:24 +0600

    refactor: update path of data_source remote directory

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -3,7 +3,7 @@ import 'package:get_it/get_it.dart';
 import 'package:dio/dio.dart';
 
 import 'package:bloc_news_app/features/daily_news/data/repository/article_repository_impl.dart';
-import 'package:bloc_news_app/features/daily_news/data/data_sources/news_api_service.dart';
+import 'package:bloc_news_app/features/daily_news/data/data_sources/remote/news_api_service.dart';
 import 'package:bloc_news_app/features/daily_news/domain/repository/article_repository.dart';
 import 'package:bloc_news_app/features/daily_news/domain/use_cases/fetch_article.dart';
 import 'package:bloc_news_app/features/daily_news/presentation/bloc/remote/remote_article_bloc.dart';
