dc8760398593d17128eaad87a3f624ea289e8462
KayesAshfaq <asfak7074@gmail.com>
2025-01-31 21:38:47 +0600

    refactor: reorder import statements for better organization

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,18 +1,17 @@
-import 'package:bloc_news_app/features/daily_news/presentation/bloc/local/local_article_bloc.dart';
 import 'package:get_it/get_it.dart';
 // ignore: depend_on_referenced_packages
 import 'package:dio/dio.dart';
 
-import 'package:bloc_news_app/features/daily_news/data/repository/article_repository_impl.dart';
-import 'package:bloc_news_app/features/daily_news/data/data_sources/remote/news_api_service.dart';
-import 'package:bloc_news_app/features/daily_news/domain/repository/article_repository.dart';
-import 'package:bloc_news_app/features/daily_news/domain/use_cases/fetch_article.dart';
-import 'package:bloc_news_app/features/daily_news/presentation/bloc/remote/remote_article_bloc.dart';
-import 'package:bloc_news_app/features/daily_news/domain/use_cases/get_saved_article.dart';
-import 'package:bloc_news_app/features/daily_news/domain/use_cases/remove_article.dart';
-import 'package:bloc_news_app/features/daily_news/domain/use_cases/save_article.dart';
-
 import 'features/daily_news/data/data_sources/local/app_database.dart';
+import 'features/daily_news/data/data_sources/remote/news_api_service.dart';
+import 'features/daily_news/data/repository/article_repository_impl.dart';
+import 'features/daily_news/domain/repository/article_repository.dart';
+import 'features/daily_news/domain/use_cases/fetch_article.dart';
+import 'features/daily_news/domain/use_cases/get_saved_article.dart';
+import 'features/daily_news/domain/use_cases/remove_article.dart';
+import 'features/daily_news/domain/use_cases/save_article.dart';
+import 'features/daily_news/presentation/bloc/local/local_article_bloc.dart';
+import 'features/daily_news/presentation/bloc/remote/remote_article_bloc.dart';
 
 final sl = GetIt.instance;
 
