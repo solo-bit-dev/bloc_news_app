9b237a6267c51859a70db38abea4f90c12685438
KayesAshfaq <asfak7074@gmail.com>
2025-01-31 21:34:16 +0600

    feat: enhance DailyNews page with article bookmarking and improved state handling

--- lib/features/daily_news/presentation/pages/home/daily_news.dart
+++ lib/features/daily_news/presentation/pages/home/daily_news.dart
@@ -1,8 +1,12 @@
 import 'dart:developer';
 
+import 'package:bloc_news_app/features/daily_news/domain/entity/article_entity.dart';
 import 'package:bloc_news_app/features/daily_news/presentation/bloc/remote/remote_article_bloc.dart';
 import 'package:bloc_news_app/features/daily_news/presentation/bloc/remote/remote_article_state.dart';
+import 'package:bloc_news_app/features/daily_news/presentation/pages/article_details/article_detail.dart';
+import 'package:bloc_news_app/features/daily_news/presentation/pages/saved_article/saved_article.dart';
 import 'package:bloc_news_app/features/daily_news/presentation/widgets/article_widget.dart';
+import 'package:bloc_news_app/features/daily_news/presentation/widgets/fallback_widget.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -10,52 +14,56 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 class DailyNews extends StatelessWidget {
   const DailyNews({super.key});
 
+  static const routeName = '/';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: _buildAppBar(),
-      body: _buildBody(),
+      appBar: AppBar(
+        title: const Text('Daily News'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.bookmark),
+            onPressed: () => _onPressBookmark(context),
+          ),
+        ],
+      ),
+      body: BlocBuilder<RemoteArticleBloc, RemoteArticleState>(
+        builder: (context, state) {
+          if (state is RemoteArticleLoadingState) {
+            return const Center(
+              child: CupertinoActivityIndicator(),
+            );
+          } else if (state is RemoteArticleErrorState) {
+            log('Error: ${state.exception?.error}');
+
+            return const Center(
+              child: Icon(Icons.refresh),
+            );
+          } else if (state is RemoteArticleLoadedState) {
+            return ListView.builder(
+              itemCount: state.articles?.length,
+              itemBuilder: (context, index) {
+                return ArticleWidget(
+                  article: state.articles![index],
+                  onArticlePressed: (article) => _onArticlePressed(context, article),
+                );
+              },
+            );
+          } else {
+            return FallbackWidget();
+          }
+        },
+      ),
     );
   }
 
-  AppBar _buildAppBar() {
-    return AppBar(
-      title: const Text('Daily News'),
-    );
+  void _onArticlePressed(BuildContext context, ArticleEntity article) {
+    log('Article pressed: ${article.title}');
+    Navigator.pushNamed(context, ArticleDetailsScreen.routeName, arguments: article);
   }
 
-  _buildBody() {
-    return BlocBuilder<RemoteArticleBloc, RemoteArticleState>(
-      builder: (context, state) {
-        if (state is RemoteArticleLoadingState) {
-          return const Center(
-            child: CupertinoActivityIndicator(),
-          );
-        }
-
-        if (state is RemoteArticleErrorState) {
-          log('Error: ${state.exception?.error}');
-
-          return const Center(
-            child: Icon(Icons.refresh),
-          );
-        }
-
-        if (state is RemoteArticleLoadedState) {
-          return ListView.builder(
-            itemCount: state.articles?.length,
-            itemBuilder: (context, index) {
-              return ArticleWidget(
-                article: state.articles![index],
-              );
-            },
-          );
-        }
-
-        return const SizedBox(
-          child: Text('data nai...'),
-        );
-      },
-    );
+  void _onPressBookmark(BuildContext context) {
+    Navigator.pushNamed(context, SavedArticleScreen.routeName);
   }
 }
